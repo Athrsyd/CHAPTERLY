fba01b29436a71d1e7eddc6af7d6b642b201ff1c
Athrysd <221656567+Athrysd@users.noreply.github.com>
2026-06-16 22:33:01 +0700

    modified:   FRONTEND/src/services/api.js

--- FRONTEND/src/services/api.js
+++ FRONTEND/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://athrsyd.pythonanywhere.com",
+    baseURL: "https://athrsyd.pythonanywhere.com/api",
     headers: {
         "Content-Type": "application/json",
     },
